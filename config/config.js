79225178e34fb86ffa37f9b3f3f374df6eb49f89
Joshua Gray <graydog.8719@gmail.com>
2024-07-02 15:49:36 -0600

    created config.js, db.js, and model.js

---
+++ config/config.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+const config = {
+  SERVER_PORT: 8001,
+  DB_NAME: "dish_sift",
+  DB_PASSWORD: process.env.DATABASE_PASSWORD,
+  DB_USER: process.env.DATABASE_USER,
+};
+
+export default config
