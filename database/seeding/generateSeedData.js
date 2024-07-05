20e1c6ca70d8223a1d5b21a6daba826d8bf61c2e
Joshua Gray <graydog.8719@gmail.com>
2024-07-05 12:27:07 -0600

    please work

--- database/seeding/generateSeedData.js
+++ database/seeding/generateSeedData.js
@@ -22,9 +22,10 @@ const allRecipes = [];
 let iterations = 0;
 
 // total number of recipes
-const count = firstRecipeData.count;
+// const count = firstRecipeData.count;
+const count = 600;
 
-const writeStream = createWriteStream("./database/seeding/all_recipes.json");
+let writeStream = createWriteStream(`./database/seeding/recipeJSONs/all_recipes${iterations}.json`);
 const jsonStream = JSONStream.stringify();
 jsonStream.pipe(writeStream);
 
@@ -73,57 +74,15 @@ const populateJSON = async (currentRecipeBatch) => {
       ingr: 1 - 100,
     },
   });
-  console.log("currentRecipeBatch:", currentRecipeBatch);
+  console.log("nextUrl:", nextUrl);
+
+  if (iterations % 10 === 0) {
+    writeStream = createWriteStream(`./database/seeding/recipeJSONs/all_recipes${iterations / 10}.json`)
+    jsonStream.pipe(writeStream)
+  }
 
   // time delay here
   setTimeout(() => populateJSON(res.data), 6667);
 };
 
 populateJSON(firstRecipeData);
-
-// const jsonTest = {key1: "hello there"}
-// const jsonTest2 = {key2: "general kenobi"}
-
-// const stringifyTest = JSON.stringify(jsonTest, null, 2)
-// const stringifyTest2 = JSON.stringify(jsonTest2, null, 2)
-
-// async function writeJsonToFile() {
-//     try {
-//         await writeFile('./database/seeding/output.json', stringifyTest);
-//         console.log('File has been written successfully.');
-//     } catch (err) {
-//         console.error('Error writing file:', err);
-//     }
-// }
-
-// writeJsonToFile()
-
-// async function writeJsonToFile2() {
-//     try {
-//         await writeFile('./database/seeding/output.json', stringifyTest2);
-//         console.log('File has been written successfully.');
-//     } catch (err) {
-//         console.error('Error writing file:', err);
-//     }
-// }
-
-// writeJsonToFile2()
-
-// const testData = 'https://api.edamam.com/api/recipes/v2?ingr=1-100&app_key=c46c2f8971618f73e9b50f94442acfa7&_cont=CHcVQBtNNQphDmgVQntAEX4BY0t7BgcCRGZFCmIRZFx6AwEAUXlSBmcQZFxxBAQBFjRDBzRBMFwmDAUBRWBGUWEbMAR7VgUVLnlSVSBMPkd5BgNK&type=public&app_id=3632ea2f'
-
-// const getRecipeData = async () => {
-//     const res = await axios.get(testData, {
-//         params: {
-//            app_id: process.env.EDAMAM_APP_ID,
-//            app_key: process.env.EDAMAM_APP_KEY,
-//            type: "public",
-//            ingr: 1-100
-//         }
-//     })
-
-//     console.log('res.data:', res.data)
-// }
-
-// getRecipeData()
-
-// console.log('testData:', testData)
