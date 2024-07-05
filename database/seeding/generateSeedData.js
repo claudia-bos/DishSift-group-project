d6afa9a09bc930bdd0c7fd6216ed3decc9abb161
Craig <craigthompson85@gmail.com>
2024-07-04 18:41:48 -0600

    Modified generateSeedData to use JSONStream and createWriteStream to write batches of 20 recipes to a json file per edaman API response

--- database/seeding/generateSeedData.js
+++ database/seeding/generateSeedData.js
@@ -1,50 +1,87 @@
-import db, { User, Favorite, Rating, Pantry, Food, Ingredient, Recipe, RecipeIngredient, Label, RecipeLabel } from "../model.js"
-import firstRecipeData from './edaman_get_recipes_v2_no_query.json' assert { type: 'json' }
-import { writeFile } from 'fs/promises'
-import axios from "axios"
+import db, {
+  User,
+  Favorite,
+  Rating,
+  Pantry,
+  Food,
+  Ingredient,
+  Recipe,
+  RecipeIngredient,
+  Label,
+  RecipeLabel,
+} from "../model.js";
+import firstRecipeData from "./edaman_get_recipes_v2_no_query.json" assert { type: "json" };
+import { writeFile } from "fs/promises";
+import { createWriteStream } from "fs";
+import JSONStream from "JSONStream";
+import axios from "axios";
 
 // array to collect all recipes
-const allRecipes = []
+const allRecipes = [];
 
-let iterations = 0
+let iterations = 0;
 
 // total number of recipes
-const count = firstRecipeData.count
+const count = firstRecipeData.count;
+
+const writeStream = createWriteStream("./database/seeding/all_recipes.json");
+const jsonStream = JSONStream.stringify();
+jsonStream.pipe(writeStream);
 
 const populateJSON = async (currentRecipeBatch) => {
-    if (currentRecipeBatch.to >= count) {
-        const seedData = JSON.stringify(allRecipes, null, 2)
-    try {
-        await writeFile('./database/seeding/all_recipes.json', seedData)
-        console.log('File has been written successfully')
-        return
-    } catch (err) {
-        console.log('Error writing file:', err)
-        return
-    }   
-    }
-    iterations++
-    console.log('iterations:', iterations)
-    currentRecipeBatch.hits.map((el) => {
-        allRecipes.push(el)
-    })
-    const nextUrl = currentRecipeBatch._links.next.href
-    const res = await axios.get(nextUrl, {
-        params: {
-           app_id: process.env.EDAMAM_APP_ID,
-           app_key: process.env.EDAMAM_APP_KEY,
-           type: "public",
-           ingr: 1-100
-        }
-    })
-    console.log('currentRecipeBatch:', currentRecipeBatch)
-    // time delay here
-    setTimeout(() => populateJSON(res.data), 6667);
-}
+  console.log("currentRecipeBatch.to: ", currentRecipeBatch.to);
+  if (currentRecipeBatch.to >= count) {
+    // const seedData = JSON.stringify(allRecipes, null, 2);
+    // try {
+    //   await writeFile("./database/seeding/all_recipes.json", seedData);
+    //   console.log("File has been written successfully");
+    //   return;
+    // } catch (err) {
+    //   console.log("Error writing file:", err);
+    //   return;
+    // }
 
-populateJSON(firstRecipeData)
+    jsonStream.end();
 
-// const jsonTest = {key1: "hello there"} 
+    writeStream.on("finish", () => {
+      console.log("Large JSON file has been written successfully.");
+    });
+
+    writeStream.on("error", (err) => {
+      console.error("Error writing large JSON file:", err);
+    });
+    console.log("All done!");
+    return;
+  }
+
+  iterations++;
+  console.log("iterations:", iterations);
+  currentRecipeBatch.hits.map((el) => {
+    allRecipes.push(el);
+  });
+
+  allRecipes.forEach((recipe) => {
+    jsonStream.write(recipe);
+  });
+
+  const nextUrl = currentRecipeBatch._links.next.href;
+  const res = await axios.get(nextUrl, {
+    params: {
+      app_id: process.env.EDAMAM_APP_ID,
+      app_key: process.env.EDAMAM_APP_KEY,
+      type: "public",
+      ingr: 1 - 100,
+    },
+  });
+  console.log("currentRecipeBatch:", currentRecipeBatch);
+
+  // time delay here
+  setTimeout(() => populateJSON(res.data), 6667);
+};
+
+populateJSON(firstRecipeData);
+
+// const jsonTest = {key1: "hello there"}
 // const jsonTest2 = {key2: "general kenobi"}
 
 // const stringifyTest = JSON.stringify(jsonTest, null, 2)
