8f697b65ba10d8dd01e22d41fa012e447c2e4f6d
Craig <craigthompson85@gmail.com>
2024-07-05 16:30:34 -0600

    Fixed generateSeedData and then ran it to generate JSON seed data files

--- database/seeding/generateSeedData.js
+++ database/seeding/generateSeedData.js
@@ -6,7 +6,7 @@ import axios from "axios";
 let iterations = 0;
 
 // total number of recipes
-const count = firstRecipeData.count;
+let count = firstRecipeData.count;
 
 let writeStream = createWriteStream(
   `./database/seeding/recipeJSONs/all_recipes_${iterations}.json`
@@ -18,56 +18,65 @@ const populateJSON = async (currentRecipeBatch) => {
   // array to hold recipes from a singe API response
   const batchRecipes = [];
 
-  console.log("currentRecipeBatch.to: ", currentRecipeBatch.to);
-  if (currentRecipeBatch.to >= count) {
-    jsonStream.end();
+  try {
+    console.log("currentRecipeBatch.to: ", currentRecipeBatch.to);
+    if (currentRecipeBatch.to >= count) {
+      jsonStream.end();
 
-    writeStream.on("finish", () => {
-      console.log("Large JSON file has been written successfully.");
+      writeStream.on("finish", () => {
+        console.log("Large JSON file has been written successfully.");
+      });
+
+      writeStream.on("error", (err) => {
+        console.error("Error writing large JSON file:", err);
+      });
+      console.log("All done!");
+      return;
+    }
+
+    iterations++;
+    console.log("iterations:", iterations);
+    currentRecipeBatch.hits.map((el) => {
+      batchRecipes.push(el);
     });
 
-    writeStream.on("error", (err) => {
-      console.error("Error writing large JSON file:", err);
+    batchRecipes.forEach((recipe) => {
+      jsonStream.write(recipe);
     });
-    console.log("All done!");
-    return;
+
+    let nextUrl = currentRecipeBatch._links.next.href;
+    nextUrl = nextUrl.replace(/(\&ingr\=\-99|\&type\=public)+/g, "");
+    console.log("nextUrl:", nextUrl);
+
+    const res = await axios.get(nextUrl, {
+      params: {
+        app_id: process.env.EDAMAM_APP_ID,
+        app_key: process.env.EDAMAM_APP_KEY,
+        type: "public",
+        ingr: 1 - 100,
+      },
+    });
+
+    count = res.data.count;
+    console.log("Available recipes count:", count);
+
+    if (iterations % 20 === 0) {
+      jsonStream.end();
+      writeStream.on("finish", () => {
+        console.log("Large JSON file has been written successfully.");
+      });
+      writeStream = createWriteStream(
+        `./database/seeding/recipeJSONs/all_recipes_${iterations / 20}.json`
+      );
+      jsonStream = JSONStream.stringify();
+      jsonStream.pipe(writeStream);
+    }
+
+    // time delay here
+    setTimeout(() => populateJSON(res.data), 6667);
+  } catch (err) {
+    console.log("***ERROR***:", err);
   }
-
-  iterations++;
-  console.log("iterations:", iterations);
-  currentRecipeBatch.hits.map((el) => {
-    batchRecipes.push(el);
-  });
-
-  batchRecipes.forEach((recipe) => {
-    jsonStream.write(recipe);
-  });
-
-  const nextUrl = currentRecipeBatch._links.next.href;
-  const res = await axios.get(nextUrl, {
-    params: {
-      app_id: process.env.EDAMAM_APP_ID,
-      app_key: process.env.EDAMAM_APP_KEY,
-      type: "public",
-      ingr: 1 - 100,
-    },
-  });
-  console.log("nextUrl:", nextUrl);
-
-  if (iterations % 20 === 0) {
-    jsonStream.end();
-    writeStream.on("finish", () => {
-      console.log("Large JSON file has been written successfully.");
-    });
-    writeStream = createWriteStream(
-      `./database/seeding/recipeJSONs/all_recipes_${iterations / 20}.json`
-    );
-    jsonStream = JSONStream.stringify();
-    jsonStream.pipe(writeStream);
-  }
-
-  // time delay here
-  setTimeout(() => populateJSON(res.data), 6667);
 };
 
 populateJSON(firstRecipeData);
