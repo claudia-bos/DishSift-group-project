99dedee7562a4692b410f314df7a6a97f3669298
Craig <craigthompson85@gmail.com>
2024-07-16 18:40:45 -0600

    Added gradient and shadow to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
   };
 
   return (
-    <header className="flex items-center justify-between p-4 bg-primary-50">
+    <header className="flex items-center justify-between p-4 bg-gradient-to-r from-primary-50 via-primary-100 to-primary-50 drop-shadow-md">
       <div className="flex-grow">
         <Link to="/">
           <h1 className="text-3xl font-bold text-center">DishSift</h1>
